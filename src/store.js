0a4650d0f151979e1cd52e1ef753dec2d038085c
Amber Brown <amberbrown@Ambers-MacBook-Pro.local>
2019-09-07 18:17:40 +0100

    add sagas

--- src/store.js
+++ src/store.js
@@ -1,7 +1,16 @@
-import { createStore } from "redux";
-import { devToolsEnhancer } from "redux-devtools-extension";
-import rootReducer from "./reducers";
+import { createStore, applyMiddleware } from "redux";
+import { composeWithDevTools } from "redux-devtools-extension";
+import createSagaMiddleware from "redux-saga";
 
-const store = createStore(rootReducer, devToolsEnhancer());
+import rootReducer from "./reducers";
+import rootSaga from "./sagas";
+
+const composeEnhancers = composeWithDevTools({});
+const sagaMiddleware = createSagaMiddleware();
+const store = createStore(
+  rootReducer,
+  composeEnhancers(applyMiddleware(sagaMiddleware))
+);
+sagaMiddleware.run(rootSaga);
 
 export default store;
